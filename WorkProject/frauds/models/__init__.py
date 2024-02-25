ef3ccfc7aa03e114b2ec29bbc94780d62a3590de
ArlanovaHelen <arlanova4e@gmail.com>
2024-02-25 10:18:00 +0200

    try tests docker

--- WorkProject/frauds/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .fraud import Fraud
-from .district import District
-
-__all__ = ['Fraud', 'District',]
--- WorkProject/frauds/models/control.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Control(models.Model):
-    num = models.CharField(max_length=220)
-    description = models.CharField(max_length=220)
-
-    def __str__(self):
-        return self.name
